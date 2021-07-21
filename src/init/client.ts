45da5a51d197122218b2d3051200521ad571f9b0
FlappyIce <flappyice73@outlook.com>
2021-07-21 01:38:15 -0400

    changed client settings

--- src/init/client.ts
+++ src/init/client.ts
@@ -2,12 +2,10 @@ import { KBotify } from 'kbotify';
 import auth from '../configs/auth';
 
 export const bot = new KBotify({
-    // mode: 'websocket',
-    mode: 'webhook',
+    mode: 'websocket',
     token: auth.khltoken,
     port: auth.khlport,
     verifyToken: auth.khlverify,
     key: auth.khlkey,
-    ignoreDecryptError: false,
-    debug: true,
+    ignoreDecryptError: true,
 });
