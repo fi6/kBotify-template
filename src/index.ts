ff2fb5414f3bc8a12c53d9625c5f119927cfff42
Lily Lu <lilyzhaoyilu@gmail.com>
2022-06-19 17:51:16 -0700

    Added comments based on recent developers questions

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { echoMenu } from './commands/echo/echo.menu';
 
 bot.messageSource.on('message', (e) => {
     bot.logger.debug(`received:`, e);
+    // 如果想要在console里查看收到信息也可以用
+    //console.log(e);
 });
 
 bot.addCommands(echoMenu);
